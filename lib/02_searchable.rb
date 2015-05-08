cc28671b378da683950aff181a1316039763639b
Marcel Soanes <m.l.soanes@gmail.com>
2015-05-08 13:20:01 -0700

    Define Searchable#where and extend SQLObject with Searchable

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,19 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_string = params.keys.map { |col| "#{col} = ?" }.join(' AND ')
+
+    parse_all(DBConnection.execute(<<-SQL, *params.values))
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_string}
+    SQL
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
